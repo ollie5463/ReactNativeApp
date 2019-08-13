0d432c134eafcd93280bd166a6ee256e6eee6042
oliver.bannister <oliver.bannister@gamesys.co.uk>
2019-08-13 13:32:44 +0100

    fixed flex's

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import { Button } from 'react-native'
 import { Database } from '../components/Database';
+import { Ionicons } from '@expo/vector-icons';
 import {
   Image,
   Platform,
@@ -15,52 +16,20 @@ import {
 import { MonoText } from '../components/StyledText';
 import Sounds from '../components/Sounds';
 
+const sounds = new Sounds();
 export default function HomeScreen() {
+  createSounds();
+  createDB();
   return (
     <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <Button title="create DB" onPress={createDB} />
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
+      <View style={styles.skipBackwardButton}>
+          <Ionicons name="ios-skip-backward" size={100}/>
+      </View>
+        <View style={styles.playButton}>
+          <Ionicons name="ios-play" size={150} onPress={playSound}/>
+      </View>
+      <View style={styles.skipButton}>
+          <Ionicons name="ios-skip-forward" size={100}/>
       </View>
     </View>
   );
@@ -70,17 +39,16 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
+function playSound() {
+  sounds.sounds.background_music.playAsync();
+}
+
+function createSounds() {
+  sounds.initSounds();
 }
 
 function createDB() {
   const databaseExample = new Database();
-  const sounds = new Sounds();
-  sounds.initSounds();
-  // sounds.sounds.background_music.playAsync();
 }
 
 
@@ -88,87 +56,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    width: 500,
+    height: 500,
+    flexDirection:"row",
+    justifyContent: 'space-between',
+    alignItems: "center"
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  playButton: {
+    flex: 1,
+    width: 150,
+    height: 150,
   },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
+  skipButton: {
+    flex: 1,
     width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+    height: 100,
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  skipBackwardButton: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    paddingLeft: 10
+  }
 });
